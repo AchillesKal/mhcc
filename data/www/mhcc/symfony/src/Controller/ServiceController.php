999fac554d16ad1fc6787e03c0b288858ce35351
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-09-04 01:52:39 +0300

    Implement /services json endpoint

--- data/www/mhcc/symfony/src/Controller/ServiceController.php
+++ data/www/mhcc/symfony/src/Controller/ServiceController.php
@@ -2,18 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Service;
+use App\Repository\ServiceRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CustomerJobController
+class ServiceController
 {
     /**
-    * @Route("/jobs", methods={"POST"})
+    * @Route("/services", methods={"GET"})
     */
-    public function newJob()
+    public function listServices(ServiceRepository $serviceRepository)
     {
-        return new Response(
-            '<html><body>Works!!</body></html>'
+        $services = $serviceRepository->findAll();
+        $data = array('services' => array());
+
+        foreach ($services as $service) {
+            $data['services'][] = $this->serializeService($service);
+        }
+
+        $response = new JsonResponse($data, 200);
+        return $response;
+    }
+
+    private function serializeService(Service $service)
+    {
+        return array(
+            'id' => $service->getId(),
+            'name' => $service->getName(),
         );
     }
 }
