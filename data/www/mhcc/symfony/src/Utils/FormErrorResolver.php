0a26688ee905e8987a537e3f8c96a93450363a37
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-09-09 18:30:53 +0300

    Add the FormErrorResolver service which get errors from forms

--- data/www/mhcc/symfony/src/Utils/FormErrorResolver.php
+++ data/www/mhcc/symfony/src/Utils/FormErrorResolver.php
@@ -1,12 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: achilleskal
- * Date: 9/9/18
- * Time: 4:01 AM
- */
+
+namespace App\Utils;
+
+use Symfony\Component\Form\FormInterface;
 
 class FormErrorResolver
 {
-
+    public function getFromErrors(FormInterface $form)
+    {
+        $errors = array();
+        foreach ($form->getErrors() as $error) {
+            $errors[] = $error->getMessage();
+        }
+        foreach ($form->all() as $childForm) {
+            if ($childForm instanceof FormInterface) {
+                if ($childErrors = $this->getFromErrors($childForm)) {
+                    $errors[$childForm->getName()] = $childErrors;
+                }
+            }
+        }
+        return $errors;
+    }
 }
